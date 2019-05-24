2168c3f1bddee647e83f7cc772b1952d67531adb
WendyNjeri <wendynjerim@gmail.com>
2019-05-24 10:59:26 +0300

    first commit

--- wild/views.py
+++ wild/views.py
@@ -15,3 +15,25 @@ def details(request, event_id):
 
     return render(request, 'wild/details.html', {'events':events})
 
+
+def update_album(request, event_id):
+    events = get_object_or_404(Event, pk=event_id)
+    events.save()
+    return request('wild:index')
+
+
+
+def delete_event(request, event_id):
+    event = get_object_or_404(Event, pk=event_id)
+    event.delete()
+    return request('wild: index')
+
+
+
+
+
+
+
+
+
+
